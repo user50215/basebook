bf6a2614ba900f9069c1c0970f88bf1307801c86
user50215 <stvnalex2005@gmail.com>
2025-06-09 18:05:49 -0700

    posts skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
+import CreatePost from "./components/CreatePost"
 import Header from "./components/Header"
+import PostFeed from "./components/PostFeed"
 
 export default function Home() {
   return (
     <div>
       <Header></Header>
+      <div className="center-menu flex flex-col items-center justify-center gap-15 mt-20">
+        <CreatePost></CreatePost>
+        <PostFeed></PostFeed>
+      </div>
     </div>
   )
 }
